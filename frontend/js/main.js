d3932a41f71ee8646afa139c66972069ec0ffca8
J.E. Remy <jeanerolle97@gmail.com>
2025-03-22 11:32:29 +0100

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,3 +2,25 @@
     placement: 'bottom',
     theme: 'light-border'
   });
+
+  // Initialize tooltips on buttons
+  tippy('#calendarButton', {
+    content: 'Remyan Calendar',
+    placement: 'bottom',
+    theme: 'light-border'
+  });
+
+  tippy('#sidebarButton', {
+    content: 'Sidebar Menu',
+    placement: 'left',
+    theme: 'light-border'
+  });
+
+  // Initialize tooltips on sidebar menu items
+  tippy('.menu-item a', {
+    content(reference) {
+      return reference.getAttribute('href').replace('pages/', '').replace('.html', '').replace(/-/g, ' ').toUpperCase();
+    },
+    placement: 'left',
+    theme: 'light-border'
+  });
